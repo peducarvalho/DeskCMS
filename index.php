1f62dd1a593661e54a4bb2bb31a1cdf703aa4816
Pedro <33048017+peducarvalho@users.noreply.github.com>
2020-07-27 12:05:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 require 'inc/sys.php';
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php $page_Head = file_get_contents('https://domain.com/inc/plugins/top_plugins.php'); echo $page_Head; ?>
+</head>
+<body>
+
+
+
+</body>
+</html>
