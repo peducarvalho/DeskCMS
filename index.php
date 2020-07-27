d092d252ac9f96e5f2f5c1daa8c962113ed12308
Pedro <33048017+peducarvalho@users.noreply.github.com>
2020-07-27 12:07:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ require 'inc/sys.php';
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<?php $page_Head = file_get_contents('https://domain.com/inc/plugins/top_plugins.php'); echo $page_Head; ?>
+<?php $page_Head = file_get_contents('https://domain.com/inc/plugins_top.php'); echo $page_Head; ?>
 </head>
 <body>
 
 
-
+<?php $page_Foot = file_get_contents('https://domain.com/inc/plugins_foot.php'); echo $page_Foot; ?>
 </body>
 </html>
